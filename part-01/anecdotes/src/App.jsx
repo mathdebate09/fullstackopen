2848a0c89213aabd540434c392e2ee51d77693fb
Jay Singh <learn.jayssingh@gmail.com>
2024-05-05 23:28:37 +0530

    part-01: exc 1.12-1.14 (anecdotes)

---
+++ part-01/anecdotes/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const randomQuote = (num) => {
+  const randomInt = Math.floor(Math.random() * num)
+  return randomInt
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length + 1).join('0').split('').map(parseFloat))
+  const [mostVoted, setMostVoted] = useState('')
+
+  const handleChangeSelected = () => {
+    setSelected(() => randomQuote(anecdotes.length))
+  }
+
+  const handleVote = (selected) => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+    const maxVotes = Math.max(...copy)
+    const index = copy.indexOf(maxVotes)
+    setMostVoted(anecdotes[index])
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={() => handleVote(selected)}>vote</button>
+      <button onClick={handleChangeSelected}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVoted}</p>
+    </div>
+  )
+}
+
+export default App
