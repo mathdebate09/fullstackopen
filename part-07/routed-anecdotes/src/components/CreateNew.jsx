2b1c81faeb41b0987e3a674ddd2f7bb2bee67690
Jay Singh <learn.jayssingh@gmail.com>
2024-06-04 17:21:09 +0530

    part-07: exc 7.4-7.6 (anecdotes)
    write customHooks

--- part-07/routed-anecdotes/src/components/CreateNew.jsx
+++ part-07/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useField } from './hooks'
 
 const CreateNew = ({ addNew, setNotification }) => {
-    const [content, setContent] = useState('');
-    const [author, setAuthor] = useState('');
-    const [info, setInfo] = useState('');
+    // const [content, setContent] = useState('');
+    // const [author, setAuthor] = useState('');
+    // const [info, setInfo] = useState('');
+
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
 
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         });
 
@@ -31,18 +36,21 @@ const CreateNew = ({ addNew, setNotification }) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+                    <input name='content' value={content.value} onChange={content.onChange} />
                 </div>
                 <div>
                     author
-                    <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input name='author' value={author.value} onChange={author.onChange} />
                 </div>
                 <div>
                     url for more info
-                    <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+                    <input name='info' value={info.value} onChange={info.onChange} />
                 </div>
                 <button>create</button>
             </form>
+            <button onClick={() => { content.reset(); author.reset(); info.reset()}}>
+                reset
+            </button>
         </div>
     )
 
